68a9f80bad31125cebaaa1741beec70903fdbd84
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-19 13:56:33 +0200

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+let number = document.getElementById('number');
+let ergebnis = document.getElementById('ergebnis');
+
+function multiplyWithTwo() {
+    console.log('multiplyWithTwo works');
+    let result = number.value * 2;
+    ergebnis.innerHTML = result;
+}
+// multiplyWithTwo(6);
+
